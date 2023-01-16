09801c83f4564300e408c0712bcce4e19929dba7
Kristina Jumanova <kristya1310@gmail.com>
2023-01-17 00:04:55 +0700

    feat: add header

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../shared/Header/Header';
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className="main">
+      <Header />
+      <div>
+        Main
+      </div>
+    </div>
+  );
+}
---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+export default function Header({ isLoggedIn = false }) {
+  return (
+    <header className="header">
+      <div className="header__logo" />
+      {!isLoggedIn ? (
+        <>
+          <a className="header__link header__link_type_sign-up" href="/">Регистрация</a>
+          <a className="header__link header__link_type_sign-in" href="/">Войти</a>
+        </>
+      )
+        : <div>Аккаунт</div>}
+
+    </header>
+  );
+}
+
+Header.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  isLoggedIn: false,
+};
