be729ec50add84af632e572dd12efbccb976af41
Kristina Jumanova <kristya1310@gmail.com>
2023-01-17 23:17:24 +0700

    feat: promo section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from '../../shared/Header/Header';
 import './Main.css';
+import Promo from './Promo/Promo';
 
 export default function Main() {
   return (
     <div className="main">
       <Header />
-      <div>
-        Main
-      </div>
+      <main>
+        <Promo />
+      </main>
     </div>
   );
 }
