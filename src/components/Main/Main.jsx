0351bc2ae9fc062725e2fdafb475388d6764b0e6
Kristina Jumanova <kristya1310@gmail.com>
2023-01-24 20:17:17 +0700

    feat(Footer): add footer section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Main.css';
 import Header from '../../shared/Header/Header';
 import AboutMe from './AboutMe/AboutMe';
 import AboutProject from './AboutProject/AboutProject';
 import Promo from './Promo/Promo';
 import Techs from './Techs/Techs';
-import './Main.css';
 import Portfolio from './Portfolio/Portfolio';
+import Footer from '../../shared/Footer/Footer';
 
 export default function Main() {
   return (
@@ -18,6 +19,7 @@ export default function Main() {
         <AboutMe />
         <Portfolio />
       </main>
+      <Footer />
     </div>
   );
 }
