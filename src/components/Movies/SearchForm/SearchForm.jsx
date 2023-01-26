73aca53b9df5b420fa69787cb6aa2515410f20fd
Kristina Jumanova <kristya1310@gmail.com>
2023-01-26 23:43:40 +0700

    feat: add search form

--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ToggleSlider from '../../../shared/ToggleSlider/ToggleSlider';
 import './SearchForm.css';
+import { ReactComponent as Magnifier } from '../../../images/lupa.svg';
 
-function SearchForm() {
+function SearchForm({ className = '' }) {
   return (
-    <form className="search-form">
+    <form className={`search-form ${className}`}>
+      <Magnifier className="search-form__magnifier" fill="#959595" />
       <input className="search-form__input" type="search" placeholder="Фильм" />
-      <button className="search-form__btn" type="submit" aria-label="Поиск" />
+      <button className="search-form__btn" type="submit" aria-label="Поиск">
+        <Magnifier className="search-form__magnifier" fill="white" />
+      </button>
       <div className="search-form__divider" />
       <ToggleSlider className="search-form__slider" />
       <h4 className="search-form__label">Короткометражки</h4>
