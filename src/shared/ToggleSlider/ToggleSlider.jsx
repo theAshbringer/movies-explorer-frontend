a26c269d25f43aa8c0abd9a67b8573bb6c048ebe
Kristina Jumanova <kristya1310@gmail.com>
2023-01-29 11:57:49 +0700

    fix(ToggleSlider): redundant htmlFor

--- src/shared/ToggleSlider/ToggleSlider.jsx
+++ src/shared/ToggleSlider/ToggleSlider.jsx
@@ -5,8 +5,8 @@ function ToggleSlider({ className = '', children, ...otherProps }) {
   return (
     <div className={`switch__container ${className}`}>
       <label className="switch" htmlFor="shortFilms">
-        <input className="switch__checkbox" id="shortFilms" name="shortFilms" type="checkbox" {...otherProps} htmlFor />
-        <div className="switch__slider" htmlFor="shortFilms" />
+        <input className="switch__checkbox" id="shortFilms" name="shortFilms" type="checkbox" {...otherProps} />
+        <div className="switch__slider" />
       </label>
       {children && <label className="switch__label" htmlFor="shortFilms">{children}</label>}
     </div>
