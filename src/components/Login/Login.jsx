d2135ccf8379aaaa6486d3a7a2d286a9659df928
Kristina Dzhumanova <dzhumanovakd@iss-reshetnev.ru>
2023-01-31 10:04:05 +0300

    feat: finish Register and Login pages

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Input from '../../shared/Input/Input';
 import Logo from '../../shared/Logo/Logo';
+import PageTitle from '../../shared/PageTitle/PageTitle';
 import SubmitSection from '../../shared/SubmitSection/SubmitSection';
 import './Login.css';
 
@@ -7,9 +9,27 @@ export default function Login() {
   return (
     <main className="login">
       <Logo className="login__logo" />
-      <h1 className="login__title">Рады видеть!</h1>
-      <input className="login__field" type="text" />
-      <input className="login__field" type="text" />
+      <PageTitle className="register__title">Рады видеть!</PageTitle>
+      <Input
+        className="login__field"
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Введите почту"
+        required
+      >
+        E-mail
+      </Input>
+      <Input
+        className="login__field"
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Введите пароль"
+        required
+      >
+        Пароль
+      </Input>
       <SubmitSection isRegistered={false} />
     </main>
   );
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Input from '../../shared/Input/Input';
 import Logo from '../../shared/Logo/Logo';
+import PageTitle from '../../shared/PageTitle/PageTitle';
 import SubmitSection from '../../shared/SubmitSection/SubmitSection';
 import './Register.css';
 
@@ -7,10 +9,37 @@ export default function Register() {
   return (
     <main className="register">
       <Logo className="register__logo" />
-      <h1 className="register__title">Добро пожаловать!</h1>
-      <input className="register__field" type="text" />
-      <input className="register__field" type="text" />
-      <input className="register__field" type="text" />
+      <PageTitle className="register__title">Добро пожаловать!</PageTitle>
+      <Input
+        className="register__field"
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Введите имя"
+        required
+      >
+        Имя
+      </Input>
+      <Input
+        className="register__field"
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Введите почту"
+        required
+      >
+        E-mail
+      </Input>
+      <Input
+        className="register__field"
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Введите пароль"
+        required
+      >
+        Пароль
+      </Input>
       <SubmitSection isRegistered />
     </main>
   );
