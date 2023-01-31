528ec87f7a0f563ee8ba8aa0be8681cc7a09d9e1
Kristina Jumanova <kristya1310@gmail.com>
2023-02-01 01:00:12 +0700

    feat(Portfolio): add valid links

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -6,9 +6,9 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <Work className="portfolio__item" link="#">Статичный сайт</Work>
-      <Work className="portfolio__item" link="#">Адаптивный сайт</Work>
-      <Work className="portfolio__item" link="#">Одностраничное приложение</Work>
+      <Work className="portfolio__item" link="https://github.com/theAshbringer/how-to-learn">Статичный сайт</Work>
+      <Work className="portfolio__item" link="https://github.com/theAshbringer/russian-travel">Адаптивный сайт</Work>
+      <Work className="portfolio__item" link="https://github.com/theAshbringer/react-mesto-auth">Одностраничное приложение</Work>
     </section>
   );
 }
