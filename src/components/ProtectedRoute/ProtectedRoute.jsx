b6be4573a35bab8d21224d715b86471612af45d4
Kristina Jumanova <kristya1310@gmail.com>
2023-02-08 01:16:23 +0700

    fix(ProtectedRoute):  change redirect url

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router';
 
 function ProtectedRoute({ isLoggedIn, children }) {
-  return isLoggedIn ? children : <Navigate to="/sign-in" replace />;
+  return isLoggedIn ? children : <Navigate to="/" replace />;
 }
 
 export default ProtectedRoute;
